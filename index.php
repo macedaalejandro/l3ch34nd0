9a9a8605c9ff4cb41689fe560b9f76b69e750472
frandesarrollador <desarrollador.frv@gmail.com>
2016-07-13 19:28:33 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+
+<?php
+		#paginar
+		$maximo=6;
+		if(!empty($_GET['pag'])){
+			$pag=limpiar($_GET['pag']);
+			
+		}else{
+			$pag=1;
+		}
+		$inicio=($pag-1)*$maximo;
+		
+			$cans=mysql_query("SELECT COUNT(campo)as total FROM tabla");
+			if($dat=mysql_fetch_array($cans)){
+				$total=$dat['total']; #inicializo la variable en 0
+			}
+?>

eda1885331f1601e35afde300fec2b4a04ed2455
macedaalejandro <maceda.alejandro@gmail.com>
2016-07-13 20:14:37 -0500

    Create navbar_out.php

---
+++ include/navbar_out.php
@@ -0,0 +1,2 @@
+	<li> <a href="http://lecheando.com/github//user_profile.php"><span class="glyphicon glyphicon-user"></span><b> Mi cuenta</b></a></li>
+   	<li> <a href="http://lecheando.com/github/function/logout.php"><span class="glyphicon glyphicon-log-in"></span><b> Salir</b></a></li>
